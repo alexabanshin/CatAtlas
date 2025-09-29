21591afc8039f8e7926b4cb9542ad093a279a2f6
abayellow <sasha.abanshin@gmail.com>
2025-09-30 08:43:16 +1100

    add initial project

---
+++ Cat Atlas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cat Atlas_UIKit
+//
+//  Created by Alexander Abanshin on 30.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
