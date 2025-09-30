fb65ebe567e81acb96a176af0c1004083e01b408
alexabanshin <Alex.Abanshin@yandex.ru>
2025-09-30 17:03:21 +1100

    System configs

--- Cat Atlas/Modules/Main/ViewController.swift
+++ Cat Atlas/Modules/Main/ViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let network = NetworkService()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        //
+        network.fetchBreeds { result in
+            switch result {
+            case .success(let breeds):
+                print(breeds[0].name)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
 
