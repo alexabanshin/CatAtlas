680155bcd2965786bebe4df61eac24c13fc40ac2
alexabanshin <Alex.Abanshin@yandex.ru>
2025-09-30 19:44:47 +1100

    Add appDependency

---
+++ Cat Atlas/AppDependency/AppDependency.swift
@@ -0,0 +1,18 @@
+//
+//  AppDependency.swift
+//  CatAtlas
+//
+//  Created by Alexander Abanshin on 30.09.2025.
+//
+
+import Foundation
+
+struct AppDependency {
+    func makeMainView() -> MainViewController {
+        let networkService = NetworkService()
+        let presenter = MainViewPresenter(networkService: networkService)
+        let view = MainViewController(presenter: presenter)
+        presenter.view = view
+        return view
+    }
+}
