c7fdbed1d5db0c4be4b154de1c4ed0af2bfae5fb
abayellow <sasha.abanshin@gmail.com>
2025-10-02 09:45:58 +1100

    add base components

---
+++ Cat Atlas/Base/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  CatAtlas
+//
+//  Created by Alexander Abanshin on 02.10.2025.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(hex: "f2e5f1")
+    }
+    
+}
---
+++ Cat Atlas/Base/BreedsColletctionViewController.swift
@@ -0,0 +1,8 @@
+//
+//  BreedsColletctionViewController.swift
+//  CatAtlas
+//
+//  Created by Alexander Abanshin on 02.10.2025.
+//
+
+import Foundation
