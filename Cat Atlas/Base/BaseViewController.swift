571f5632dc32518920fd71d5446c26703fc36d0d
abayellow <sasha.abanshin@gmail.com>
2025-10-02 12:46:07 +1100

    Configure base classes

--- Cat Atlas/Base/BaseViewController.swift
+++ Cat Atlas/Base/BaseViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class BaseViewController: UIViewController {
+    let customNavBar = CustomNavBar()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(hex: "f2e5f1")
+        setupCustomNavBar()
     }
     
+    private func setupCustomNavBar() {
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        view.addSubview(customNavBar)
+        customNavBar.backgroundColor = .clear
+        
+        NSLayoutConstraint.activate([
+            customNavBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            customNavBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            customNavBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            customNavBar.heightAnchor.constraint(equalToConstant: 100),
+        ])
+    }
 }
---
+++ Cat Atlas/Base/CustomNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  CustomNavigationController.swift
+//  CatAtlas
+//
+//  Created by Alexander Abanshin on 02.10.2025.
+//
+
+import UIKit
+
+class CustomNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
