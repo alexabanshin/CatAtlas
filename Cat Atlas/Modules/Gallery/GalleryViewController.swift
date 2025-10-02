385e8ca2aae16602fc38d1d295e916d77c2bc7d5
abayellow <sasha.abanshin@gmail.com>
2025-10-02 12:47:36 +1100

    Modify GalleryViewController

--- Cat Atlas/Modules/Gallery/GalleryViewController.swift
+++ Cat Atlas/Modules/Gallery/GalleryViewController.swift
@@ -7,24 +7,18 @@
 
 import UIKit
 
-class GalleryViewController: UIViewController {
+class GalleryViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = "Image"
+        configureView()
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureView() {
+        customNavBar.titleLabel.text = "Gallery"
+        customNavBar.imageView.image = UIImage(named: "cat")
     }
-    */
 
 }
