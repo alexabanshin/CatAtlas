bcc902384a8e719c54881f00e4c6890e13693809
abayellow <sasha.abanshin@gmail.com>
2025-10-02 12:49:36 +1100

    Modify ProfileViewController

--- Cat Atlas/Modules/Profile/ProfileViewController.swift
+++ Cat Atlas/Modules/Profile/ProfileViewController.swift
@@ -7,25 +7,19 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = "Profile"
-        view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        configureView()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureView() {
+        customNavBar.titleLabel.text = "Profile"
+        customNavBar.imageView.image = UIImage(named: "cat")
     }
-    */
+
 
 }
